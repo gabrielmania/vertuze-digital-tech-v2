58b3a1fa9e1a92e897d317f06675f29f03c0ee8b
Gabriel Mania <mania.gabb@gmail.com>
2023-02-24 00:02:35 +0800

    added server.js, added routes for /movies and /series

--- server/server.js
+++ server/server.js
@@ -6,13 +6,22 @@ const Series = require("./models/series");
 
 const app = express();
 
+// Connect to database
 connectDb();
 
+// @notice /movies routes will get all the movies in the database
 app.get("/movies", async (req, res) => {
   const movies = await Movie.find();
   res.send(movies);
 });
 
+// @notice /series routes will get all the series in the database
+app.get("/series", async (req, res) => {
+  const series = await Series.find();
+  res.send(series);
+});
+
+// Listen on port 3000
 app.listen(3000, () => {
   console.log("Listening on port 3000...");
 });

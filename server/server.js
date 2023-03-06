f7c90f8627bd634a64d404360410c1570c66d4b0
Gabriel Mania <mania.gabb@gmail.com>
2023-03-06 15:02:50 +0800

    added cors package for server

--- server/server.js
+++ server/server.js
@@ -1,17 +1,19 @@
 const express = require("express");
+const cors = require("cors");
 const connectDb = require("./utils/connectDb");
 const Movie = require("./models/movie");
 const Series = require("./models/series");
 
 const app = express();
 
+// Provide access to all cors
+app.use(cors());
+
 // Connect to database
 connectDb();
 
 // @notice /movies route will get all the movies in the database.
 app.get("/movies", async (req, res) => {
-  // Set Access-Control-Allow-Origin to "*" to allow request from any domain.
-  res.set("Access-Control-Allow-Origin", "*");
   const movies = await Movie.find();
   res.send(movies);
 });
@@ -19,16 +21,12 @@ app.get("/movies", async (req, res) => {
 // @notice /movies:movieId route will get the detials for the specified movie.
 app.get("/movies/:movieId", async (req, res) => {
   const { movieId } = req.params;
-  // Set Access-Control-Allow-Origin to "*" to allow request from any domain.
-  res.set("Access-Control-Allow-Origin", "*");
   const movie = await Movie.findOne({ _id: movieId });
   res.send(movie);
 });
 
 // @notice /series route will get all the series in the database.
 app.get("/series", async (req, res) => {
-  // Set Access-Control-Allow-Origin to "*" to allow request from any domain.
-  res.set("Access-Control-Allow-Origin", "*");
   const series = await Series.find();
   res.send(series);
 });
@@ -36,8 +34,6 @@ app.get("/series", async (req, res) => {
 // @notice /series:seriesId route will get the detials for the specified series.
 app.get("/series/:seriesId", async (req, res) => {
   const { seriesId } = req.params;
-  // Set Access-Control-Allow-Origin to "*" to allow request from any domain.
-  res.set("Access-Control-Allow-Origin", "*");
   const series = await Series.findOne({ _id: seriesId });
   res.send(series);
 });
